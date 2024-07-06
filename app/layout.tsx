eab100b3ecc1454643f51358b7554310f62e8b09
Igor Matheus <pessoal@igormatheus.com.br>
2024-07-06 15:57:30 -0300

    my first blog post

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ import { baseUrl } from './sitemap'
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Next.js Portfolio Starter',
-    template: '%s | Next.js Portfolio Starter',
+    default: 'Igor Matheus',
+    template: '%s | Igor Matheus',
   },
   description: 'This is my portfolio.',
   openGraph: {
--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,9 @@ export default function Page() {
         My Portfolio
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`I'm a professional with a degree in Nutrition and experience in healthcare management. 
+        Passionate about human physiology and technology, bringing a unique blend of scientific knowledge and leadership abilities to drive organizational success and innovation.
+        `}
       </p>
       <div className="my-8">
         <BlogPosts />
